e8f57d159c56ad7223a50ba9b277cece2f0859a8
droiddikshit <akshayadikshit1004@gmail.com>
2023-01-25 00:55:55 +0530

    xml changes

--- app/src/main/java/com/akshaya/newsapp/ui/countries/CountrySelectionAdapter.kt
+++ app/src/main/java/com/akshaya/newsapp/ui/countries/CountrySelectionAdapter.kt
@@ -4,12 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.akshaya.newsapp.databinding.ActivityNewsSourceItemBinding
+import com.akshaya.newsapp.databinding.LanguageSelectionItemBinding
 
 class CountrySelectionAdapter(
     private val languageData: ArrayList<HashMap<String?, String?>>
 ) : RecyclerView.Adapter<CountrySelectionAdapter.DataViewHolder>() {
 
-    class DataViewHolder(private val binding: ActivityNewsSourceItemBinding) :
+    class DataViewHolder(private val binding: LanguageSelectionItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(holder: ArrayList<HashMap<String?, String?>>) {
             binding.languageNames.text = holder.get(position).get("name")
@@ -18,7 +19,7 @@ class CountrySelectionAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         DataViewHolder(
-            ActivityNewsSourceItemBinding.inflate(
+            LanguageSelectionItemBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
