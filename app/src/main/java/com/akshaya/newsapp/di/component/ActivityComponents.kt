9a15b797de7814a418d9c2a9958f7b76fbed6c81
droiddikshit <akshayadikshit1004@gmail.com>
2023-01-25 00:23:28 +0530

    ActivityComponents changes

--- app/src/main/java/com/akshaya/newsapp/di/component/ActivityComponents.kt
+++ app/src/main/java/com/akshaya/newsapp/di/component/ActivityComponents.kt
@@ -2,6 +2,7 @@ package com.akshaya.newsapp.di.component
 
 import com.akshaya.newsapp.di.ActivityScope
 import com.akshaya.newsapp.di.module.ActivityModule
+import com.akshaya.newsapp.ui.countries.CountriesSelectionActivity
 import com.akshaya.newsapp.ui.homescreen.HomeScreenActivity
 import com.akshaya.newsapp.ui.newssource.NewsSourceActivity
 import com.akshaya.newsapp.ui.newssource.sourcedetails.NewsSourceDetailsActivity
@@ -16,5 +17,6 @@ interface ActivityComponent {
     fun inject(activity: HomeScreenActivity)
     fun inject(activity: NewsSourceActivity)
     fun inject(activity: NewsSourceDetailsActivity)
+    fun inject(activity: CountriesSelectionActivity)
 
 }
