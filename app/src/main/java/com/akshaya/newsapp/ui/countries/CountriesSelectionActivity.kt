b2d60998dd44d7267008fa0460bad2f499679bc4
droiddikshit <akshayadikshit1004@gmail.com>
2023-01-26 01:59:08 +0530

    Country selection activity changes

--- app/src/main/java/com/akshaya/newsapp/ui/countries/CountriesSelectionActivity.kt
+++ app/src/main/java/com/akshaya/newsapp/ui/countries/CountriesSelectionActivity.kt
@@ -1,11 +1,14 @@
 package com.akshaya.newsapp.ui.countries
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.akshaya.newsapp.databinding.NewsLanguageSelectionBinding
 import com.akshaya.newsapp.di.component.ActivityComponent
 import com.akshaya.newsapp.ui.base.BaseActivity
+import com.akshaya.newsapp.ui.countrydetails.CountryDetailsActivity
+import com.akshaya.newsapp.utils.AppConstants
 import com.akshaya.newsapp.utils.ReadJsonHelper.loadJSONFromAssets
 import org.json.JSONArray
 import org.json.JSONObject
@@ -32,19 +35,19 @@ class CountriesSelectionActivity : BaseActivity(), CountrySelectionAdapter.Count
     fun loadFromJSONFile() {
         try {
             val obj = JSONObject(loadJSONFromAssets(this))
-            val status: String = obj.getString("status")
-            if (status == "success") {
+            val status: String = obj.getString(AppConstants.STATUS)
+            if (status == AppConstants.SUCCESS) {
                 val contactArray: JSONArray = obj.getJSONArray("data")
                 val countryList: ArrayList<HashMap<String?, String?>> = ArrayList()
                 var countryData: HashMap<String?, String?>
                 for (i in 0 until contactArray.length()) {
                     val contacts = contactArray.getJSONObject(i)
-                    val name = contacts.getString("name")
+                    val name = contacts.getString(AppConstants.NAME)
                     countryData = HashMap()
-                    countryData["name"] = name
+                    countryData[AppConstants.NAME] = name
                     countryList.add(countryData)
                 }
-                val contactAdapter = CountrySelectionAdapter(countryList,this)
+                val contactAdapter = CountrySelectionAdapter(countryList, this)
                 binding.recyclerView.setLayoutManager(LinearLayoutManager(this))
                 binding.recyclerView.setAdapter(contactAdapter)
                 binding.progressBar.visibility = View.GONE
@@ -59,6 +62,9 @@ class CountriesSelectionActivity : BaseActivity(), CountrySelectionAdapter.Count
     }
 
     override fun onCountrySelectionListner(data: String) {
-
+        startActivity(Intent(this,CountryDetailsActivity::class.java).putExtra(
+            AppConstants.LANGUAGE_CODE_KEY,
+            data
+        ))
     }
 }
