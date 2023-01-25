c17cc5f442f23a30fc72d20502ac53baa8431c90
droiddikshit <akshayadikshit1004@gmail.com>
2023-01-26 02:04:58 +0530

    fragment temp changes

--- app/src/main/java/com/akshaya/newsapp/di/module/FragmentModule.kt
+++ app/src/main/java/com/akshaya/newsapp/di/module/FragmentModule.kt
@@ -1,9 +1,24 @@
 package com.akshaya.newsapp.di.module
 
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akshaya.newsapp.data.repository.TopHeadlineRepository
 import com.akshaya.newsapp.ui.base.BaseFragment
+import com.akshaya.newsapp.ui.base.ViewModelProviderFactory
+import com.akshaya.newsapp.ui.countrydetails.FragmentViewModel
 import dagger.Module
+import dagger.Provides
 
 @Module
 class FragmentModule(private val fragment: BaseFragment) {
+    @Provides
+    fun provideLinearLayoutManager(): LinearLayoutManager = LinearLayoutManager(fragment.context)
+
+    @Provides
+    fun provideHomeLanguageSelectionViewModel(topHeadlineRepository: TopHeadlineRepository): FragmentViewModel {
+        return ViewModelProvider(fragment,
+            ViewModelProviderFactory(FragmentViewModel::class) {
+                FragmentViewModel(topHeadlineRepository)
+            })[FragmentViewModel::class.java]
+    }
 }
---
+++ app/src/main/java/com/akshaya/newsapp/ui/countrydetails/FragmentViewModel.kt
@@ -0,0 +1,7 @@
+package com.akshaya.newsapp.ui.countrydetails
+
+import com.akshaya.newsapp.data.repository.TopHeadlineRepository
+import com.akshaya.newsapp.ui.base.BaseViewModel
+
+class FragmentViewModel(private val topHeadlineRepository: TopHeadlineRepository) : BaseViewModel() {
+}
