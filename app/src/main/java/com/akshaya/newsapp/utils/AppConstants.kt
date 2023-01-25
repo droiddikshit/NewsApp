3b34d19b5c07c57798841329c28932f4ae2f3989
droiddikshit <akshayadikshit1004@gmail.com>
2023-01-26 01:57:06 +0530

    Constant changes

--- app/src/main/java/com/akshaya/newsapp/utils/AppConstants.kt
+++ app/src/main/java/com/akshaya/newsapp/utils/AppConstants.kt
@@ -7,6 +7,7 @@ object AppConstants {
     const val API_KEY = BuildConfig.API_KEY
     const val COUNTRY = "in"
     const val NEWS_SRC_KEY = "news_src_key"
+    const val LANGUAGE_CODE_KEY = "lang_code_key"
     const val SUCCESS = "success"
     const val NAME = "name"
     const val STATUS = "status"
