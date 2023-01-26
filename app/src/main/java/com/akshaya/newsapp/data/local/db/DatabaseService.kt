db38006160a2c258faa0d5f9084bede09c97df00
droiddikshit <akshayadikshit1004@gmail.com>
2023-01-26 19:48:59 +0530

    added room dependency

---
+++ app/src/main/java/com/akshaya/newsapp/data/local/db/DatabaseService.kt
@@ -0,0 +1,9 @@
+package com.akshaya.newsapp.data.local.db
+
+import androidx.room.RoomDatabase
+import com.akshaya.newsapp.data.local.db.dao.NewsDao
+
+abstract class DatabaseService: RoomDatabase() {
+
+    abstract fun dummyDao(): NewsDao
+}
---
+++ app/src/main/java/com/akshaya/newsapp/data/local/db/dao/NewsDao.kt
@@ -0,0 +1,16 @@
+package com.akshaya.newsapp.data.local.db.dao
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+
+@Entity(tableName = "dummy_entity")
+data class NewsDao(
+    @PrimaryKey(autoGenerate = true)
+    @NotNull
+    val id: Long, @ColumnInfo(name = "name")
+    @NotNull
+    val name: String
+)
