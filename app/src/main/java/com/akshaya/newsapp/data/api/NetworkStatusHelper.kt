b239760f4fe2d82b76fd82d34ee5f0bff3ee0299
droiddikshit <akshayadikshit1004@gmail.com>
2023-02-07 01:56:34 +0530

    NetworkStatusHelper added

---
+++ app/src/main/java/com/akshaya/newsapp/data/api/NetworkStatusHelper.kt
@@ -0,0 +1,26 @@
+package com.akshaya.newsapp.data.api
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+
+class NetworkStatusHelper(val context: Context) {
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isNetworkConnected(): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                return true
+            }
+        }
+        return false
+    }
+}
